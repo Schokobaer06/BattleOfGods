2bd6fb02d68d85fc1a922c49f67d5376c7d4d388
Schokobaer006 <josef.schokobaer@gmail.com>
2025-03-16 22:32:44 +0100

    fixed Copperbroadsword + animation

--- src/main/java/com/schokobaer/battleofgods/item/CopperBroadswordItem.java
+++ src/main/java/com/schokobaer/battleofgods/item/CopperBroadswordItem.java
@@ -1,18 +1,13 @@
 
 package com.schokobaer.battleofgods.item;
 
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class CopperBroadswordItem extends SwordItem {
 	public CopperBroadswordItem() {
@@ -51,12 +46,5 @@ public class CopperBroadswordItem extends SwordItem {
 	@Override
 	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
 		return new ItemStack(this);
-	}
-
-	@Override
-	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, level, list, flag);
-		list.add(Component.translatable("item.battleofgods.copper_broadsword.description_0"));
-		list.add(Component.translatable("item.battleofgods.copper_broadsword.description_1"));
 	}
 }
