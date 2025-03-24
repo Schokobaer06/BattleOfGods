b3b56749047229bcac2fb5293a1d3a5c4f483f33
Josef Peer <josef.schokobaer@gmail.com>
2025-03-24 08:57:16 +0100

    added Overrides for Item, SwordItem, TieredItem

---
+++ src/main/java/com/schokobaer/battleofgods/mechanics/item/override/TieredItem.java
@@ -0,0 +1,25 @@
+package com.schokobaer.battleofgods.mechanics.item.override;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
+
+public class TieredItem extends Item {
+    private final Tier tier;
+
+    public TieredItem(Tier p_43308_, Item.Properties p_43309_) {
+        super(p_43309_.defaultDurability(p_43308_.getUses()));
+        this.tier = p_43308_;
+    }
+
+    public Tier getTier() {
+        return this.tier;
+    }
+
+    public int getEnchantmentValue() {
+        return this.tier.getEnchantmentValue();
+    }
+
+    public boolean isValidRepairItem(ItemStack p_43311_, ItemStack p_43312_) {
+        return this.tier.getRepairIngredient().test(p_43312_) || super.isValidRepairItem(p_43311_, p_43312_);
+    }
+}
