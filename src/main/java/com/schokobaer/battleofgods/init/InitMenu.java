e5224968f7ce92478482af01f9a3752e536d15de
Josef Peer <josef.schokobaer@gmail.com>
2025-03-27 10:57:41 +0100

    .

---
+++ src/main/java/com/schokobaer/battleofgods/init/InitMenu.java
@@ -0,0 +1,15 @@
+package com.schokobaer.battleofgods.init;
+
+import com.schokobaer.battleofgods.BattleofgodsMod;
+import com.schokobaer.battleofgods.world.inventory.WorkbenchMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class InitMenu {
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, BattleofgodsMod.MODID);
+    public static final RegistryObject<MenuType<WorkbenchMenu>> WORKBENCH = MENUS.register("workbench", () -> IForgeMenuType.create(WorkbenchMenu::new));
+
+}
--- src/main/java/com/schokobaer/battleofgods/world/inventory/WorkbenchMenu.java
+++ src/main/java/com/schokobaer/battleofgods/world/inventory/WorkbenchMenu.java
@@ -1,18 +1,27 @@
 package com.schokobaer.battleofgods.world.inventory;
 
+import com.schokobaer.battleofgods.init.InitMenu;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class WorkbenchMenu extends AbstractContainerMenu implements Supplier<Map<Integer, Slot>> {
-    protected WorkbenchMenu(@Nullable MenuType<?> p_38851_, int p_38852_) {
-        super(p_38851_, p_38852_);
+    public final Level world;
+    public final Player entity;
+
+    public WorkbenchMenu(int containerId, Inventory playerInv, FriendlyByteBuf data) {
+        super(InitMenu.WORKBENCH.get(), containerId);
+        this.world = playerInv.player.level();
+        this.entity = playerInv.player;
     }
 
     @Override
