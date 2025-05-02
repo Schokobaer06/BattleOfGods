0b9c6f19e6be58baa1933930b275186f3abb55c2
Josef Peer <josef.schokobaer@gmail.com>
2025-05-02 12:50:51 +0200

    add WoodArmor class with defense attributes and type-based functionality

---
+++ src/main/java/com/schokobaer/battleofgods/armor/tier1/WoodArmor.java
@@ -0,0 +1,29 @@
+package com.schokobaer.battleofgods.armor.tier1;
+
+import com.schokobaer.battleofgods.armor.TerrariaArmorItem;
+import com.schokobaer.battleofgods.init.InitRarity;
+import com.schokobaer.battleofgods.init.InitTier;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class WoodArmor extends TerrariaArmorItem {
+
+    public WoodArmor(ArmorMaterial material, Type type, Properties properties) {
+        super(
+                material,
+                type,
+                properties,
+                getDefenseForType(type),  // Defense-Wert basierend auf Typ
+                InitRarity.WHITE,// Deine Registry für Raritäten
+                InitTier.TIER_1           // Deine Registry für Tiers
+        );
+    }
+
+    private static int getDefenseForType(Type type) {
+        return switch (type) {
+            case HELMET -> 0;
+            case CHESTPLATE -> 1;
+            case LEGGINGS -> 1;
+            case BOOTS -> 0;
+        };
+    }
+}
