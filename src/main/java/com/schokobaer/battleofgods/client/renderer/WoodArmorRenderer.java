ac4f5bd56f39e1f6f19c38547622d26faf4af928
Schokobaer006 <josef.schokobaer@gmail.com>
2025-05-03 21:47:49 +0200

    Versimpelung von WoodArmorRenderer

--- src/main/java/com/schokobaer/battleofgods/client/renderer/WoodArmorRenderer.java
+++ src/main/java/com/schokobaer/battleofgods/client/renderer/WoodArmorRenderer.java
@@ -2,20 +2,11 @@ package com.schokobaer.battleofgods.client.renderer;
 
 import com.schokobaer.battleofgods.armor.tier1.WoodArmor;
 import com.schokobaer.battleofgods.armor.tier1.WoodArmorModel;
-import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class WoodArmorRenderer extends GeoArmorRenderer<WoodArmor> {
     public WoodArmorRenderer() {
         super(new WoodArmorModel());
-        this.head = new GeoBone(null, "armorHead", false, (double) 0, false, false);
-        this.body = new GeoBone(null, "armorBody", false, (double) 0, false, false);
-        this.rightArm = new GeoBone(null, "armorRightArm", false, (double) 0, false, false);
-        this.leftArm = new GeoBone(null, "armorLeftArm", false, (double) 0, false, false);
-        this.rightLeg = new GeoBone(null, "armorRightLeg", false, (double) 0, false, false);
-        this.leftLeg = new GeoBone(null, "armorLeftLeg", false, (double) 0, false, false);
-        this.rightBoot = new GeoBone(null, "armorRightBoot", false, (double) 0, false, false);
-        this.leftBoot = new GeoBone(null, "armorLeftBoot", false, (double) 0, false, false);
     }
 /*
     @Override
